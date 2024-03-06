e5176af8d1d09a5cf99142bed8755b8289ba30a0
xiaomibenit <158188746+xiaomibenit@users.noreply.github.com>
2024-03-06 15:35:28 +0100

    Delete firebase-app.js

--- firebase-app.js
+++
@@ -1,7 +0,0 @@
-const firebase = require('firebase/app');
-require('firebase/storage');
-
-const FirebaseApp = firebase.app;
-const initializeApp = firebase.initializeApp;
-
-module.exports = { FirebaseApp, initializeApp };
